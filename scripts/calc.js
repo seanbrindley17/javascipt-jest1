778f3533ef190f39afc51e3af8b5eebe8e153e17
Sean Brindley <seanbrindley17@gmail.com>
2024-06-04 16:32:02 +0100

    Add failing test and subsequent passing code

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,16 @@
-function addition() {       // A trivial funtion that just returns 42
-    return 42;              // Is just enough code to get the test to pass
+function addition(num1, num2) {     // To pass the test where other numbers are returned, have to add simple addition like this   
+    return num1 + num2;              
 }
 
 module.exports = addition;      // Exports the function so that the require statement in the test file works
+
+// Test 1
+// function addition() {       // A trivial funtion that just returns 42
+//     return 42;              // Is just enough code to get the test to pass
+// }
+
+
+// Test 2
+// function addition(num1, num2) {     // To pass the test where other numbers are returned, have to add simple addition like this   
+//     return num1 + num2;              
+// }
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 
 // If test file is below the js file being tested in directory, need to use .. to connect it
 const addition = require("../calc");        // Import function that is being tested from the calc.js file. Assign at constant so it doesn't get overwritten
@@ -7,7 +10,11 @@ describe("Calculator", () => {       // We want to describe the calculator tests
     describe("Addition function", () => {       // We want to describe the addition function
         test("should return 42 for 20 + 22", () => {        // Describe the test
             expect(addition(20, 22)).toBe(42);      // We expect that when the addition function is called with numbers 20 & 42, it will return 42
-        })                                          // .toBe is the same as using === to check for value and type 
+        });                                         // .toBe is the same as using === to check for value and type            
+        
+        test("should return 73 for 42 + 31", () => {        // Create test that intentionally fails. 
+            expect(addition(42, 31)).toBe(73);              // Function currently only returns 42, so any other return will fail it
+        })                                           
     });
 
     describe("Subtraction function", () => {
